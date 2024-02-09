8213160457b0a4b0b2f458e23285ac6751431bc7
Onadelyer <tokarmacs63@gmail.com>
2024-02-09 17:28:39 +0200

    1

--- clear-environment.sh
+++ clear-environment.sh
@@ -4,22 +4,23 @@ stop_all_containers() {
     running_containers=$(docker ps -q)
     if [ -n "$running_containers" ]; then
         docker stop $running_containers
+    fi
 }
 
 remove_all_containers() {
     all_containers=$(docker ps -aq)
     if [ -n "$all_containers" ]; then
         docker rm $all_containers
+    fi
 }
 
 remove_all_images() {
     all_images=$(docker images -aq)
     if [ -n "$all_images" ]; then
         docker rmi $all_images
+    fi
 }
 
 stop_all_containers
-
 remove_all_containers
-
 remove_all_images
