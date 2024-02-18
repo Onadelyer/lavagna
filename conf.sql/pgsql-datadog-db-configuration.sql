405cd68120f1e1f0823b392f79a68730bfe197ad
Onadelyer <tokarmacs63@gmail.com>
2024-02-18 16:29:02 +0200

    1

--- conf.sql/pgsql-datadog-db-configuration.sql
+++ conf.sql/pgsql-datadog-db-configuration.sql
@@ -1,45 +1,37 @@
--- Create the Datadog user with a specified password
-CREATE USER datadog WITH PASSWORD 'datadog';
+CREATE USER datadog WITH password 'datadog';
 
--- Enable the pg_stat_statements extension globally
+CREATE SCHEMA datadog;
+GRANT USAGE ON SCHEMA datadog TO datadog;
+GRANT USAGE ON SCHEMA public TO datadog;
+GRANT SELECT ON pg_stat_database TO datadog;
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 
--- Grant the Datadog user read-only access to necessary system views
-GRANT USAGE ON SCHEMA pg_catalog TO datadog;
-GRANT SELECT ON pg_stat_activity TO datadog;
-GRANT SELECT ON pg_stat_statements TO datadog;
-
--- Additional permissions for Datadog to access database-wide stats
-GRANT SELECT ON pg_stat_database TO datadog;
-
--- Optional: Create a schema for Datadog (if you plan to use custom metrics or need additional isolation)
-CREATE SCHEMA IF NOT EXISTS datadog;
-GRANT USAGE ON SCHEMA datadog TO datadog;
-
--- Create or replace functions to expose pg_stat_activity and pg_stat_statements
--- Note: These are basic versions and might need adjustments based on your security requirements
-
--- Function to expose pg_stat_activity
 CREATE OR REPLACE FUNCTION datadog.pg_stat_activity() RETURNS SETOF pg_stat_activity AS
-$$
-SELECT * FROM pg_catalog.pg_stat_activity;
-$$
-LANGUAGE sql VOLATILE SECURITY DEFINER;
-GRANT EXECUTE ON FUNCTION datadog.pg_stat_activity() TO datadog;
-
--- Function to expose pg_stat_statements
+  $$ SELECT * FROM pg_catalog.pg_stat_activity; $$
+LANGUAGE sql
+SECURITY DEFINER;
 CREATE OR REPLACE FUNCTION datadog.pg_stat_statements() RETURNS SETOF pg_stat_statements AS
-$$
-SELECT * FROM public.pg_stat_statements;
-$$
-LANGUAGE sql VOLATILE SECURITY DEFINER;
-GRANT EXECUTE ON FUNCTION datadog.pg_stat_statements() TO datadog;
+    $$ SELECT * FROM pg_stat_statements; $$
+LANGUAGE sql
+SECURITY DEFINER;
 
--- Ensure the Datadog user cannot access sensitive data
-REVOKE ALL ON SCHEMA public FROM datadog;
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM datadog;
-REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM datadog;
-REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM datadog;
+CREATE OR REPLACE FUNCTION datadog.explain_statement(
+   l_query TEXT,
+   OUT explain JSON
+)
+RETURNS SETOF JSON AS
+$$
+DECLARE
+curs REFCURSOR;
+plan JSON;
 
--- Apply the changes
-COMMIT;
+BEGIN
+   OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+   FETCH curs INTO plan;
+   CLOSE curs;
+   RETURN QUERY SELECT plan;
+END;
+$$
+LANGUAGE 'plpgsql'
+RETURNS NULL ON NULL INPUT
+SECURITY DEFINER;
