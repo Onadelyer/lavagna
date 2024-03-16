f123803f3959b1fe5f4a269e0b4c5fab952dc788
Onadelyer <tokarmacs63@gmail.com>
2024-03-16 23:00:08 +0200

    1

--- terraform/modules/beanstalk/main.tf
+++ terraform/modules/beanstalk/main.tf
@@ -1,39 +1,10 @@
-resource "aws_elastic_beanstalk_application" "beanstalk_application" {
-    name = "lavagna"
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
 }
 
-resource "aws_iam_instance_profile" "beanstalk_instance_profile" {
-  name = "beanstalk-instance-profile"
-  role = aws_iam_role.beanstalk_role.name
-}
-
-resource "aws_iam_role" "beanstalk_role" {
-  name               = "beanstalk-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy_attachment" "beanstalk_policy_attachment" {
-  name       = "beanstalk-policy-attachment"
-  roles      = [aws_iam_role.beanstalk_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-}
-
-resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
-  name                = "lavagna-environment"
-  application         = aws_elastic_beanstalk_application.beanstalk_application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.6.4 running Corretto 8"
-  description         = "Environment for Java8 application \"lavagna\""
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
 }
