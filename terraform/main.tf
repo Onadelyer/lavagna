ed4aa6f08ddacc65870305b3347f2bae33fbaabf
Onadelyer <tokarmacs63@gmail.com>
2024-03-20 21:42:56 +0200

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,80 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.41.0"
+      source = "hashicorp/aws"
+      version = "5.41.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-west-2"
 }
 
-module "beanstalk" {
-  source = "./modules/beanstalk"
+resource "aws_elastic_beanstalk_application" "application" {  
+  name = "lavagna"
+  description = "Java 8 application \"lavagna\""
+}
+
+# data "archive_file" "bundle" {
+#   type        = "jar"
+#   source_dir  = "./Build"
+#   output_path = "bundle.jar"
+# }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "lavagna-app-bucket"
+}
+
+resource "aws_s3_object" "bucket_object" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "beanstalk/bundle.war"
+  # source = data.archive_file.bundle.output_path
+  source = "./Build/lavagna-jetty-console.war"
+}
+
+resource "aws_elastic_beanstalk_application_version" "name" {
+  name        = "test-version"
+  application = aws_elastic_beanstalk_application.application.name
+  bucket      = aws_s3_bucket.bucket.bucket
+  key         = aws_s3_object.bucket_object.key
+}
+
+resource "aws_elastic_beanstalk_environment" "name" {
+  name                = "lavagna-env"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2 v4.4.1 running Tomcat 9 Corretto 8"
+  version_label = aws_elastic_beanstalk_application_version.name.name
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+
+  setting {
+    namespace = "aws:elb:listener:8080"
+    name      = "ListenerProtocol"
+    value     = "HTTP"
+  }
+  setting {
+    namespace = "aws:elb:listener:8080"
+    name      = "InstancePort"
+    value     = "8080"
+  }
+  setting {
+    namespace = "aws:elb:listener:8080"
+    name      = "InstanceProtocol"
+    value     = "HTTP"
+  }
+  setting {
+    namespace = "aws:elb:listener:8080"
+    name      = "ListenerEnabled"
+    value     = "true"
+  }
 }
---
+++ terraform/sample.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample PHP Page</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a sample PHP page deployed through AWS Elastic Beanstalk.</p>
+</body>
+</html>
