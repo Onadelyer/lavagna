30e6a81ae3b44587a19cdd698821b2fdb0a27dbd
Максим <maksim@Noutbuk-Maksim.local>
2024-03-21 01:26:33 +0200

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,23 +118,23 @@ data "archive_file" "bundle" {
   output_path = "bundle.zip"
 }
 
-resource "aws_db_instance" "demobank_rds" {
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t2.micro"
-  identifier             = "javademoapp"
-  db_name                = "demobank_db"
-  username               = "admin"
-  password               = "adminuser"
-  parameter_group_name   = "default.mysql8.0"
-  multi_az               = false
-  publicly_accessible    = false
-  skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.demoapp_db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.bank_app_rds_sg.id]
-}
+# resource "aws_db_instance" "demobank_rds" {
+#   allocated_storage      = 20
+#   storage_type           = "gp2"
+#   engine                 = "mysql"
+#   engine_version         = "8.0"
+#   instance_class         = "db.t2.micro"
+#   identifier             = "javademoapp"
+#   db_name                = "demobank_db"
+#   username               = "admin"
+#   password               = "adminuser"
+#   parameter_group_name   = "default.mysql8.0"
+#   multi_az               = false
+#   publicly_accessible    = false
+#   skip_final_snapshot    = true
+#   db_subnet_group_name   = aws_db_subnet_group.demoapp_db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.bank_app_rds_sg.id]
+# }
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "lavagna-app-bucket"
