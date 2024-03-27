845b8895c65e8d54bae564454d0f205f7899c93b
Максим <maksim@Noutbuk-Maksim.local>
2024-03-27 02:40:59 +0200

    1

--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -14,7 +14,7 @@ resource "aws_codebuild_project" "name" {
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:corretto8"
     type            = "LINUX_CONTAINER"
   }
 
